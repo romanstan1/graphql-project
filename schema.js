0100b67408536b087e48fe6fb686502c66b316a0
Roman Stankiewicz <roman.stan3@gmail.com>
2017-08-16 15:39:00 +0100

    graphql server running, hardcoded customer data

--- schema.js
+++ schema.js
@@ -7,6 +7,42 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.export = new GraphQLSchema({
+const customers = [
+  {id:1, name: "John Smith", email: 'john@email.com', age: 35},
+  {id:2, name: "Jane Smith", email: 'jane@hotmail.com', age: 29},
+  {id:3, name: "Steve Smith", email: 'steve@gmail.com', age: 22}
+];
 
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt}
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customers: {
+      type: CustomerType,
+      args:{
+        id: {type: GraphQLString}
+      },
+      resolve(parentValue, args) {
+        for(let i = 0; i< customers.length; i++) {
+          if(customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+
+module.export = new GraphQLSchema({
+  query: RootQuery
 });
